75dbd496601045a95db7b3176607f2226dc356f5
Andrew Smith <andrew@andrew.codes>
2017-11-19 21:55:04 -0500

    Include publish nps command

--- package-scripts.js
+++ package-scripts.js
@@ -1,8 +1,11 @@
+import {crossEnv} from 'nps';
+
 module.exports = {
     scripts: {
         build: `babel src --out-dir .`,
         'build/watch': `babel src --watch --out-dir .`,
         default: `nps build/watch`,
+        publish: crossEnv(`NODE_ENV=production nps build && npm publish`),
         test: 'echo "Error: no test specified" && exit 1'
     }
 };
