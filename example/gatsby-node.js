54aeff72b40f0cc2ad12cd8b88da08ac2e02716e
Benjamin Abel <dev.abel@laposte.net>
2018-11-01 18:16:33 +0100

    Fix eslint error consistent-return
    https://eslint.org/docs/rules/consistent-return

--- example/gatsby-node.js
+++ example/gatsby-node.js
@@ -1,36 +1,40 @@
 const path = require(`path`)
 
-exports.createPages = ({ actions, graphql }) => {
+exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
-  const blogPostTemplate = path.resolve(`src/templates/md.js`)
-
-  return graphql(`
-    {
-      allMarkdownRemark(
-        sort: { order: DESC, fields: [frontmatter___path] }
-        limit: 1000
-      ) {
-        edges {
-          node {
-            frontmatter {
-              path
+  return new Promise((resolve, reject) => {
+    const blogPostTemplate = path.resolve(`src/templates/md.js`)
+    graphql(`
+      {
+        allMarkdownRemark(
+          sort: { order: DESC, fields: [frontmatter___path] }
+          limit: 1000
+        ) {
+          edges {
+            node {
+              frontmatter {
+                path
+              }
             }
           }
         }
       }
-    }
-  `).then(result => {
-    if (result.errors) {
-      return Promise.reject(result.errors)
-    }
+    `).then(result => {
+      if (result.errors) {
+        reject(result.errors)
+      }
 
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-      createPage({
-        path: node.frontmatter.path,
-        component: blogPostTemplate,
-        context: {}, // additional data can be passed via context
+      // Create blog posts pages.
+      result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+        createPage({
+          path: node.frontmatter.path, // required
+          component: blogPostTemplate,
+          context: {},
+        })
       })
+
+      resolve()
     })
   })
 }
